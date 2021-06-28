626918b59f806fe28f2b75f84190be0ca66eb1a9
esiomlink <simon.ecoffet@gmail.com>
2021-06-29 01:11:49 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
+import logo from './img/fiverr.png'
 
 function App() {
   return (
     <div className='App'>
+      <div className='logo'>
+        <img src={logo} alt='logo' />
+      </div>
+
       <Dashboard />
     </div>
   );
